dcfc4d7487dc7007a173db5bdefee55927f16961
bibs2091 <m.naas@esi-sba.dz>
2021-07-12 18:36:13 +0100

    Add recommendItem function

--- app.mjs
+++ app.mjs
@@ -1,6 +1,6 @@
 import * as tf from '@tensorflow/tfjs-node'
-
-function createModel(numMovies = 1683, numUsers = 944) {
+const numUsers = 944; const numMovies = 1683;
+function createModel(numUsers = 944, numMovies = 1683) {
   const userInputLayer = tf.input({ shape: 1, dtype: "int32", name: "user" });
   const userEmbeddingLayer = tf.layers.embedding({
     inputDim: numUsers + 1,
@@ -47,55 +47,29 @@ function createDataset(csvFile = "data.csv") {
         dtype: "float32"
       }
     }
-  })).batch(512)
-  return csvDataset;
+  })).batch(1024)
+  const dataSet = csvDataset.map(x => ({ xs: { user: x.xs.user.reshape([-1, 1]), movie: x.xs.movie.reshape([-1, 1]) }, ys: x.ys }))
+  return dataSet;
 }
 
-async function trainModel(model, dataSet) {
+async function trainModel(model, dataSet, epochsNum = 1) {
   console.log("Start training");
-  let val = await model.fitDataset(dataSet, {
-    epochs: 10,
+  await model.fitDataset(dataSet, {
+    epochs: epochsNum,
   });
 }
 
-async function mmm() {
-  const model = await createModel();
-  const dataSet = await createDataset();
-
-  // dataSet.filter(x => x.ys.rating === 3 ).toArray().then(a => console.log(a));
-  // await dataSet.toArray().then(a => console.log(a.xs.user.dataSync()));
-  const dataSet2 = await dataSet.map(x => ({ xs: { user: x.xs.user.reshape([-1, 1]), movie: x.xs.movie.reshape([-1, 1]) }, ys: x.ys }))
-  // await dataSet2.forEachAsync(e => console.log(e.xs.user));
-
-  // dataSet2.toArray().then(a => console.log(a));
-  await trainModel(model, dataSet2);
+function recommendItem(userId, model) {
+  let toPredict = [tf.fill([numMovies, 1], userId), tf.range(0, numMovies).reshape([-1, 1])]
+  return model.predictOnBatch(toPredict).argMax();
 }
 
-mmm()
 
+async function nn() {
+  const model = createModel();
+  const dataSet = createDataset();
+  await trainModel(model, dataSet);
+  await recommendItem(9, model).print();
+}
 
-// async function m() {
-//   const xArray = [
-//     [1, 1, 1, 1, 1, 1, 1, 1, 1],
-//     [1, 1, 1, 1, 1, 1, 1, 1, 1],
-//     [1, 1, 1, 1, 1, 1, 1, 1, 1],
-//     [1, 1, 1, 1, 1, 1, 1, 1, 1],
-//   ];
-//   const yArray = [1, 1, 1, 1];
-//   const xDataset = tf.data.array(xArray);
-//   const yDataset = tf.data.array(yArray);
-//    const xyDataset = tf.data.zip({ xs: xDataset, ys: yDataset })
-//      .batch(4)
-//      xyDataset.toArray().then(a => console.log(a));
-
-//    const model = tf.sequential({
-//      layers: [tf.layers.dense({ units: 1, inputShape: [9] })]
-//    });
-//    model.compile({ optimizer: 'sgd', loss: 'meanSquaredError' });
-//    const history = await model.fitDataset(xyDataset, {
-//      epochs: 4,
-//      callbacks: { onEpochEnd: (epoch, logs) => console.log(logs.loss) }
-//    });
-
-//  }
- //  m()
+nn()

b79acfc7049801bd71fa60431df35b1cbe718f98
bibs2091 <m.naas@esi-sba.dz>
2021-09-09 01:08:07 +0100

    init knn model

---
+++ pprec/src/knn.ts
@@ -0,0 +1,10 @@
+import * as tf from '@tensorflow/tfjs-node'
+import { DataBlock } from './DataBlock'
+
+export class knn {
+
+
+    constructor(dataBlock: DataBlock,usersNum: number, itemsNum: number) {
+
+    }
+}
